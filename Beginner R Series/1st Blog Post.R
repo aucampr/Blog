4e82653f9b02fc4f60ad23cda6cb7209db6d758b
Ruan Aucamp <aucampr@gmail.com>
2023-04-03 12:27:39 +0200

    Create 1st Blog Post.R
    Added 1st Blog Post

---
+++ Beginner R Series/1st Blog Post.R
@@ -0,0 +1,48 @@
+# Addition
+15 + 17
+
+# Multiplication
+2 * 7
+
+# Division
+32 / 4 
+
+#Square Root
+sqrt(25)
+
+#Exponents
+4^2
+
+#Remainders
+25 %% 3
+
+# Assign a variable
+road1 <- "D1174"
+road2 = "D3671"
+
+class(road1)
+
+# Print the variable value with the print function
+print(road1)
+
+#Print the variable value by only typing the variable name
+road2
+
+# Calculate a road length
+start_km <- 11.6
+end_km <- 17.2
+
+class(start_km)
+
+length <- end_km - start_km
+length
+
+# Create a variable called active and assign a value
+active <- TRUE
+class(active)
+
+# Variables are case sensitive
+print(active)
+
+print(Active)
+
